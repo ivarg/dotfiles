309447ec8e757b40961aec8a61c698944fec9855
Ivar Gaitan <ivar.gaitan@gmail.com>
2017-09-01 08:31:19 +0200

    Add vim makefile

--- Makefile
+++ Makefile
@@ -7,11 +7,10 @@ DOTFILES ?= ${DOT_HOME}/dotfiles
 .PHONY: vim 
 .SILENT: vim
 
-help: ## Display this help message
+help: 
 	@cat $(MAKEFILE_LIST) | grep -e "^[a-zA-Z_\-]*: *.*## *" | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
 
-vim: ## Set up neovim
-	echo "Setting up vim with ${SHELL}"
 
 include zsh/zsh.mk
+include vim/vim.mk
 
---
+++ vim/vim.mk
@@ -0,0 +1,34 @@
+# Install and set up neovim
+# Dependencies: Homebrew, git
+
+VIM_DIR = ${DOTFILES}/vim
+
+# TODO: setup mattn/gist-vim, see :h gist-vim-setup
+# TODO: set ctrl-h to send escape sequence in iterm prefs (https://github.com/neovim/neovim/issues/2048#issuecomment-98307896) [workaround for bug]
+
+.SILENT: ctags powerline vim_plug
+
+ctags:
+	echo ">>> Installing ctags..."
+	brew install ctags
+
+powerline:
+	echo ">>> Installing powerline fonts..."
+	$(if $(wildcard ${VIM_DIR}/fonts), rm -rf ${VIM_DIR}/fonts,)
+	git -C ${VIM_DIR} clone https://github.com/powerline/fonts.git --depth=1
+	${VIM_DIR}/fonts/install.sh
+	rm -rf ${VIM_DIR}/fonts
+
+vim_plug:
+	echo ">>> Installing vim plug..."
+	curl -fLo ${DOT_HOME}/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+neovim:
+	echo ">>> Installing neovim..."
+	brew install neovim
+
+vim: neovim ctags powerline vim_plug ## Set up a vim environment
+	echo "> Setting up neovim"
+	ln -s ${DOTFILES}/vim/vimrc ${DOT_HOME}/.config/nvim/init.vim 
+
