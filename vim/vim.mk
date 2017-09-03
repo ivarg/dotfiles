96dd3b276a55a8a30a40e9fc2c2933f95f5ff1b8
Ivar Gaitan <ivar.gaitan@gmail.com>
2017-09-03 14:49:44 +0200

    Mute neovim recipe

--- vim/vim.mk
+++ vim/vim.mk
@@ -6,7 +6,7 @@ VIM_DIR = ${DOTFILES}/vim
 # TODO: setup mattn/gist-vim, see :h gist-vim-setup
 # TODO: set ctrl-h to send escape sequence in iterm prefs (https://github.com/neovim/neovim/issues/2048#issuecomment-98307896) [workaround for bug]
 
-.SILENT: ctags powerline vim_plug
+.SILENT: ctags powerline vim_plug neovim
 
 ctags:
 	echo ">>> Installing ctags..."
@@ -29,6 +29,6 @@ neovim:
 	brew install neovim
 
 vim: neovim ctags powerline vim_plug ## Install and set up Neovim
-	echo "> Setting up neovim"
+	echo ">>> Setting up vimrc"
 	ln -s ${DOTFILES}/vim/vimrc ${DOT_HOME}/.config/nvim/init.vim 
 
