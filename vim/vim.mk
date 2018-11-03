4e5736cd9d82e1cb65cc70961c85f45e55294269
Ivar Gaitan <ivar.gaitan@tv4.se>
2018-11-03 22:07:30 +0100

    Install plugins as well

--- vim/vim.mk
+++ vim/vim.mk
@@ -1,7 +1,8 @@
 # Install and set up neovim
 # Dependencies: Homebrew, git
 
-VIM_DIR = ${DOTFILES}/vim
+VIM_DIR := ${DOTFILES}/vim
+VIM_CONFIG := ${DOT_HOME}/.config/nvim
 
 # TODO: setup mattn/gist-vim, see :h gist-vim-setup
 # TODO: set ctrl-h to send escape sequence in iterm prefs (https://github.com/neovim/neovim/issues/2048#issuecomment-98307896) [workaround for bug]
@@ -9,26 +10,29 @@ VIM_DIR = ${DOTFILES}/vim
 .SILENT: ctags powerline vim_plug neovim
 
 ctags:
-	echo ">>> Installing ctags..."
+	echo "> Installing ctags"
 	brew install ctags
 
 powerline:
-	echo ">>> Installing powerline fonts..."
+	echo "> Installing powerline fonts"
 	$(if $(wildcard ${VIM_DIR}/fonts), rm -rf ${VIM_DIR}/fonts,)
 	git -C ${VIM_DIR} clone https://github.com/powerline/fonts.git --depth=1
 	${VIM_DIR}/fonts/install.sh
 	rm -rf ${VIM_DIR}/fonts
+	echo "> Done installing powerline fonts"
 
 vim_plug:
-	echo ">>> Installing vim plug..."
-	curl -fLo ${DOT_HOME}/.config/nvim/autoload/plug.vim --create-dirs \
+	echo "> Installing vim plug"
+	curl -fLo ${VIM_CONFIG}/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	nvim +PlugInstall +q
 
 neovim:
-	echo ">>> Installing neovim..."
+	echo "> Installing neovim"
 	brew install neovim
+	echo "> Done installing neovim"
 
 vim: neovim ctags powerline vim_plug ## Install and set up Neovim
-	echo ">>> Setting up vimrc"
-	ln -s ${DOTFILES}/vim/vimrc ${DOT_HOME}/.config/nvim/init.vim 
+	echo "> Linking init.vim"
+	ln -s ${DOTFILES}/vim/vimrc ${VIM_CONFIG}/init.vim 
 
