ef51d1ec794565934b870dd45d27efcf21231099
Ivar Gaitan <ivar.gaitan@tv4.se>
2018-11-03 21:42:24 +0100

    Fix git.mk resilience

--- git/git.mk
+++ git/git.mk
@@ -6,5 +6,8 @@ GIT_DIR = ${DOTFILES}/git
 .SILENT: git
 
 git: ## Set up git
+	[ -e ${DOT_HOME}/.gitignore ] && rm ${DOT_HOME}/.gitignore
+	[ -e ${DOT_HOME}/.gitconfig ] && rm ${DOT_HOME}/.gitconfig
+	echo "> Linking gitignore and gitconfig"
 	ln -s ${GIT_DIR}/gitignore ${DOT_HOME}/.gitignore
 	ln -s ${GIT_DIR}/gitconfig ${DOT_HOME}/.gitconfig
