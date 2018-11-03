f31cd27fd0662b59433d19430a59ae42f3af8df0
Ivar Gaitan <ivar.gaitan@gmail.com>
2018-11-03 21:35:39 +0100

    Add resilience to Karabiner setup

--- karabiner/karabiner.mk
+++ karabiner/karabiner.mk
@@ -2,7 +2,11 @@
 
 .PHONY: karabiner
 .SILENT: karabiner
+KARABINER_CONFIG := ${DOT_HOME}/.config/karabiner
+KARABONER_FILE := ${KARABINER_CONFIG}/karabiner.json 
 
 karabiner: ## Set up Karabiner
 	mkdir -p ${DOT_HOME}/.config/karabiner
-	ln -s ${DOTFILES}/karabiner/karabiner.json "${DOT_HOME}/.config/karabiner"
+	[ -e ${KARABINER_CONFIG}/karabiner.json ] && rm ${KARABINER_CONFIG}/karabiner.json
+	echo "> Linking Karabiner config to ${KARABINER_CONFIG}/karabiner.json"
+	ln -s ${DOTFILES}/karabiner/karabiner.json "${KARABINER_CONFIG}"
