428619aeb7c3a207df23967574837a1c29d98761
Ivar Gaitan <ivar.gaitan@gmail.com>
2018-11-07 09:50:19 +0100

    Let brew bundle find a Brewfile

--- brew/brew.mk
+++ brew/brew.mk
@@ -6,6 +6,6 @@
 
 brew_install:
 	/usr/bin/ruby -e "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	brew bundle
+	brew bundle --file=${DOTFILES}/brew/Brewfile
 
 brew: brew_install ## Install Homebrew
