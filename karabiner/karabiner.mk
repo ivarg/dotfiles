443ac3b0a0e7c2b94fef50b48548d98552eae37e
Ivar Gaitan <ivar.gaitan@gmail.com>
2018-11-07 10:02:33 +0100

    Fix karabiner.mk

--- karabiner/karabiner.mk
+++ karabiner/karabiner.mk
@@ -2,11 +2,11 @@
 
 .PHONY: karabiner
 .SILENT: karabiner
-KARABINER_CONFIG := ${DOT_HOME}/.config/karabiner
-KARABONER_FILE := ${KARABINER_CONFIG}/karabiner.json 
+KARABINER_CONFIG = ${DOT_HOME}/.config/karabiner
+KARABONER_FILE = ${KARABINER_CONFIG}/karabiner.json 
 
 karabiner: ## Set up Karabiner
 	mkdir -p ${DOT_HOME}/.config/karabiner
-	[ -e ${KARABINER_CONFIG}/karabiner.json ] && rm ${KARABINER_CONFIG}/karabiner.json
+	rm -f ${KARABINER_CONFIG}/karabiner.json
 	echo "> Linking Karabiner config to ${KARABINER_CONFIG}/karabiner.json"
 	ln -s ${DOTFILES}/karabiner/karabiner.json "${KARABINER_CONFIG}"
