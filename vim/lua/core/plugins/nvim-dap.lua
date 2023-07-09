12844c239ebce8e24b7beda51ebdd7ad4767c2e0
Ivar Gaitan <ivar.gaitan@gmail.com>
2023-07-09 21:37:08 +0200

    wip

--- vim/lua/core/plugins/nvim-dap.lua
+++ vim/lua/core/plugins/nvim-dap.lua
@@ -2,7 +2,7 @@ vim.keymap.set('n', '<leader>dd', function() require('dap').continue() end)
 vim.keymap.set('n', '<leader>dn', function() require('dap').step_over() end)
 vim.keymap.set('n', '<leader>di', function() require('dap').step_into() end)
 vim.keymap.set('n', '<leader>do', function() require('dap').step_out() end)
-vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
 vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
 
