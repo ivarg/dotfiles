171bafdd2d7db9e6911504ede1b3e074f6989c0e
Ivar Gaitan <ivar.gaitan@gmail.com>
2023-07-20 19:44:02 +0200

    Treesitter

--- vim/lua/core/plugins/treesitter.lua
+++ vim/lua/core/plugins/treesitter.lua
@@ -1,24 +1,46 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    enabled = false,
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "python", "rust", "toml" },
-            sync_install = false,
-            auto_install = true,
-            build = ":TSUpdate",
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
+    -- enabled = false,
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        ensure_installed = {
+            "bash",
+            "dockerfile",
+            "html",
+            "markdown",
+            "markdown_inline",
+            "org",
+            "query",
+            "regex",
+            "latex",
+            "vim",
+            "vimdoc",
+            "yaml",
+            "lua",
+            "python",
+            "rust",
+            "toml"
+        },
+        sync_install = false,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = {
+            enable = true,
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "gnn", -- set to `false` to disable one of the mappings
+                node_incremental = "grn",
+                scope_incremental = "grc",
+                node_decremental = "grm",
             },
-            indent = {
-                enable = true,
-            },
-            rainbow = {
-                enable = true,
-                extended_mode = true,
-                max_file_lines = nil,
-            }
-        })
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
     end,
 }
