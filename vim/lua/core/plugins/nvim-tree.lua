7acec1ef5b77cd6872cdb72f8ac397d4c95f846b
Ivar Gaitan <ivar.gaitan@embark-studios.com>
2023-10-04 15:20:12 +0200

    Nvim-tree improvement

--- vim/lua/core/plugins/nvim-tree.lua
+++ vim/lua/core/plugins/nvim-tree.lua
@@ -1,34 +1,34 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.api.nvim_set_keymap('n', '<C-e>', '<cmd>NvimTreeToggle<cr>', {})
+vim.api.nvim_set_keymap("n", "<C-e>", "<cmd>NvimTreeFindFileToggle<cr>", {})
 
 return {
-    'nvim-tree/nvim-tree.lua',
+    "nvim-tree/nvim-tree.lua",
     name = "nvim-tree",
-    cmd = { 'NvimTreeToggle', 'NvimTreeOpen' },
-    dependencies = {'nvim-tree/nvim-web-devicons'},
+    cmd = { "NvimTreeFindFileToggle", "NvimTreeOpen" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-      sort_by = "case_sensitive",
-      renderer = {
-        group_empty = true
-      },
+        sort_by = "case_sensitive",
+        renderer = {
+            group_empty = true,
+        },
     },
     config = function()
         local M = {}
         local api = require("nvim-tree.api")
         local function opts(desc, bufnr)
-            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
         end
         function M.on_attach(bufnr)
             api.config.mappings.default_on_attach(bufnr)
 
-            vim.keymap.set('n', '<C-e>', api.tree.toggle, opts('Close tree', bufnr))
-            vim.keymap.set('c', '<C-c>', '<Nop>', opts('Close tree', bufnr))
+            vim.keymap.set("n", "<C-e>", api.tree.toggle, opts("Close tree", bufnr))
+            vim.keymap.set("c", "<C-c>", "<Nop>", opts("Close tree", bufnr))
         end
+
         require("nvim-tree").setup({
             on_attach = M.on_attach,
         })
     end,
 }
-
